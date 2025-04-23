f2d7ec789477e3fbeb410336189eaf53aac072e6
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2025-04-23 12:52:29 +0300

    Init

---
+++ src/main/java/ru/maverick/excelsearcher/ExcelSearcherApplication.java
@@ -0,0 +1,13 @@
+package ru.maverick.excelsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelSearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maverick/excelsearcher/ExcelSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maverick.excelsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
